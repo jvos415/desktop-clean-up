8f29595de7b913831a52cb37e1a78a56efd61be5
Jvos <john.voskuyl@gmail.com>
2024-12-23 14:49:15 -0800

    completed script

--- clean-up-desktop-screenshots.sh
+++ clean-up-desktop-screenshots.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# Set the base directory
-DESKTOP_DIR=~/Desktop
-NEW_DIRNAME="screenshots-$(date +%Y-%m-%d)"
+# set the dir to cleanup and new dir name for today's date
+DIR_TO_CLEANUP=~/Desktop
+NEW_DIR_NAME="screenshots-$(date +%Y-%m-%d)"
 
-# Check if current date dir exists, if not, create it
+# check if current date dir exists, if not, create it
 ensure_dir_exists() {
     local dir_name="$1"
-    if [ ! -d "dir_name"]; then
+    if [ ! -d "$dir_name"]; then
         mkdir "$dir_name"
     fi
     echo "$dir_name"
 }
 
+# main function that actually does the cleanup
 clean_up_screenshots() {
-    
+    cd "$DIR_TO_CLEANUP" || { echo "Failed to access directory to clean up"; exit 1; }
+
+    TARGET_DIR=$(ensure_dir_exists "$NEW_DIR_NAME")
+
+    for file in Screenshot*; do
+        if [ -e "$file" ]; then
+            mv "$file" "$TARGET_DIR"
+        fi
+    done
+    echo "Moved screenshots into: $TARGET_DIR"
 }
+
+# run the script
+clean_up_screenshots
